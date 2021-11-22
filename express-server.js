42db4f1f1c0c98c7a29f5dd0532c9257593a691f
itachi00000 <58568276+itachi00000@users.noreply.github.com>
2021-11-22 19:32:20 +0800

    add mw logger, display all friends, and messages

--- express-server.js
+++ express-server.js
@@ -5,43 +5,88 @@ const app = express();
 
 const PORT = 3000;
 
-const friends = [
-{
+const friends = [{
 	id:1,name: 'Tesla', 
-},
-{
-	id:2, name: "Newton"
-},
-{
-	id:3, name: "Einstein"
-}]
+},{
+	id:2, name: "Einstein"
+},{
+	id:3, name: "Newton"
+}
+]
 
+// use w/ next
+// logging middleware
+app.use((req, res, next) => {
+	const start = Date.now()
 
+	next();
+	const delta = Date.now() - start;
+
+	// basic log. ex. GET / 4ms
+	console.log(`${req.method}  ${req.url}  ${delta}ms`)
+})
+
+// GET - /
 app.get('/', (req, res)=> {
 
-	res.send('hello')
+// .send for html, text, etc
+	res.send('main page')
 
 })
 
+// GET - /friends - return all friends
 app.get('/friends', (req, res)=> {
 
-	res.send('friends route')
+if(friends.length > 0){
+// .json for data
+	return res.json(friends)
+}
+
+return res.status(404).json({
+		error: "no friend list"
+	})
 
 })
 
+// GET - /friend/:id - a friend
+app.get('/friends/:friendId', (req, res)=> {
+
+// const friendId = req.params.friendId
+	const { friendId } = req.params
+
+	const friendData = friends[Number(friendId) - 1] 
+
+	if(friendData) {
+		// .json for data
+		// dont forget the return
+		return res.json(friendData)
+	} 
+
+
+	return res.status(404).json({
+		error: "friend not exist"
+	})
+	
+
+	
+
+})
+
+// GET - /messages - return all messages
 app.get('/messages', (req, res)=> {
 
 	res.send('messages route')
 
 })
 
-// post
 
+// POST - /messages
 app.post('/messages', (req, res)=> {
 	res.send('POST messages route')
 
 })
 
+// POST - /friends
 app.post('/friends', (req, res)=> {
 	res.send('POST friends route')
 
